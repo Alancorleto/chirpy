0c913b1091bdf8b72e589a80ccfa6f24b69e880f
alancorleto <corletoalan@gmail.com>
2025-11-17 16:10:47 -0300

    Integrate SQLC and create first users query

--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/alancorleto/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Printf("Error creating SQL database: %v\n", err)
+		return
+	}
+	dbQueries := database.New(db)
+
 	const filepathRoot = "."
 	const port = "8080"
 
@@ -21,7 +37,9 @@ func main() {
 		Addr:    ":" + port,
 	}
 
-	apiCfg := apiConfig{}
+	apiCfg := apiConfig{
+		dbQueries: dbQueries,
+	}
 
 	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	serveMux.HandleFunc("GET /api/healthz", healthzResponse)
@@ -30,7 +48,7 @@ func main() {
 	serveMux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		fmt.Printf("Error listening to server: %v\n", err)
 	}
